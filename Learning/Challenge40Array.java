523e92a754ccf233403f63a8aca03b3a43a5853a
Piyush Raj <121041810+Piyushraj2002@users.noreply.github.com>
2026-08-05 23:50:37 +0530

    Update Challenge40Array.java

--- Learning/Challenge40Array.java
+++ Learning/Challenge40Array.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
+
 public class Challenge40Array {
     public static void main(String[] args) {
         System.out.println("Welcome to the Array");
         System.out.println("Find the sum and average of all elements in an array.");
         //int[] myARrray = {1,2,3,4,5,6,7,8,9};
-      // later we will add how to take input using scannaer
-        int[] myArray = new int[10];
+        /*int[] myArray = new int[10];
         myArray[0] = 1;
         myArray[1] = 2;
         myArray[2] = 3;
@@ -14,7 +15,15 @@ public class Challenge40Array {
         myArray[6] = 7;
         myArray[7] = 8;
         myArray[8] = 9;
-        myArray[9] = 10;
+        myArray[9] = 10;*/
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of element: ");
+        int size = input.nextInt();
+        int[] myArray = new int[size];
+        for (int i=0;i<size;i++){
+            System.out.print("Enter element at"+(i)+": ");
+            myArray[i] = input.nextInt();
+        }
         int sum = sumOfArray(myArray);
         System.out.println("Sum of the given arraqy is: " + sum);
         double avg = Average(myArray);
